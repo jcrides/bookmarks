9dc24c23fc15dcb904ab01a3396586e9bfe21adf
jcrides <jeremycar@gmail.com>
2015-03-07 22:56:21 -0800

    show all folders and links on welcome index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+
+  def index
+    @folders = Folder.all.sort_by { |folder| folder.name }
+    @links = Link.all.sort_by { |link| link.name }
+  end
+  
 end
