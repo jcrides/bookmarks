8ff484b409ba4d3265e054afbf0837a1a9e749b5
jcrides <jeremycar@gmail.com>
2015-03-08 10:10:07 -0700

    only show links not in folder on welcome index view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @folders = Folder.all.sort_by { |folder| folder.name }
-    @links = Link.all.sort_by { |link| link.name }
+    @links = Link.where('folder_id is NULL').sort_by { |link| link.name }
   end
-  
+
 end
